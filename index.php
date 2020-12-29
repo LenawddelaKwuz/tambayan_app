1ecb7978dfe494e3632bd9f0b868995560451a6f
Berlenard M. Dela Cruz <60724928+LenawddelaKwuz@users.noreply.github.com>
2020-12-29 16:16:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+Include_once("C:\Users\Berlenard\Desktop\New web\Services\service.html");
+Include_once("C:\Users\Berlenard\Desktop\New web\Services\style.css");
+Include_once("C:\Users\Berlenard\Desktop\New web\Home\index.html");
+Include_once("C:\Users\Berlenard\Desktop\New web\Home\style.css");
+Include_once("C:\Users\Berlenard\Desktop\New web\Contact\info.html");
+Include_once("C:\Users\Berlenard\Desktop\New web\Contact\style.css");
+Include_once("C:\Users\Berlenard\Desktop\New web\About\about.html");
+Include_once("C:\Users\Berlenard\Desktop\New web\Aboutstyle.css");
+
+?>
